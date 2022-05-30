74340b519926852eb76f76e4a44dde8be9025afd
Fabrizio Saullo <fabriziodsaullo@gmail.com>
2022-05-30 16:57:38 -0300

    cards validations

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,9 @@
 class Card < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :type, presence: true
+  validates :rarity, presence: true
+  validates :image, presence: true
+  validates :price, presence: true
 end
