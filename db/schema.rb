91c33a07e7d2f82b66cc08ab69c9c2b3fc8b8db6
Fabrizio Saullo <fabriziodsaullo@gmail.com>
2022-05-30 16:33:53 -0300

    Fix cards reference

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,18 @@ ActiveRecord::Schema.define(version: 2022_05_30_181413) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "cards", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.string "rarity"
+    t.string "image"
+    t.decimal "price", precision: 10, scale: 2
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_cards_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -27,4 +39,5 @@ ActiveRecord::Schema.define(version: 2022_05_30_181413) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cards", "users"
 end
