5992f4384cf429db1d659c5ae25fccb333d54c7e
Fabrizio Saullo <fabriziodsaullo@gmail.com>
2022-05-31 11:24:04 -0300

    Add some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first) 
+# Populate your users first
+
+all_cards = Pokemon::Card.where(page: 1, pageSize: 5)
+
+all_cards.each do |card|
+  name = card.name
+  category = card.types.first
+  rarity = card.rarity
+  image = card.images.large
+  price = 9.99
+  user_id = rand(1..5)
+  Card.create(name: name, category: category, rarity: rarity, image: image, price: price, user_id: user_id)
+end
