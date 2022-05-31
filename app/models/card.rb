0cf026acaaf5083b93cc4a8a557655a23d0f1688
Fabrizio Saullo <fabriziodsaullo@gmail.com>
2022-05-31 10:22:55 -0300

    Changed type to categoty from card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,7 +2,7 @@ class Card < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true
-  validates :type, presence: true
+  validates :category, presence: true
   validates :rarity, presence: true
   validates :image, presence: true
   validates :price, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,15 +4,4 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-all_cards = Pokemon::Card.where(page: 1, pageSize: 151)
-
-all_cards.each do |card|
-  name = card.name
-  type = card.types
-  rarity = card.rarity
-  image = card.images.large
-  price = 99.99
-  Card.new(name: name, type: type, rarity: rarity, image: image, price: price, user_id: 1)
-end
+#   Character.create(name: 'Luke', movie: movies.first) 
