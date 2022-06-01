280e16d140dea204c3fda02c80c3f1efe51a72fd
Fabrizio Saullo <fabriziodsaullo@gmail.com>
2022-06-01 16:13:37 -0300

    Order working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :cards
-  resources :orders, only: [:create]
+  resources :cards do
+    resources :orders, only: [:create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
