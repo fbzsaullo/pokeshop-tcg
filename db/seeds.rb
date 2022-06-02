a95ce62568f347d26f022edb2d70cc1a37e0e0c1
Erick Valente <erickhwk@gmail.com>
2022-06-02 11:05:14 -0300

    footer and show button margin issues fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
 # Populate your users first
 
-all_cards = Pokemon::Card.where(page: 1, pageSize: 5)
+# all_cards = Pokemon::Card.where(page: 1, pageSize: 5)
 
-all_cards.each do |card|
-  name = card.name
-  category = card.types.first
-  rarity = card.rarity
-  image = card.images.large
-  price = 9.99
-  user_id = rand(1..5)
-  Card.create(name: name, category: category, rarity: rarity, image: image, price: price, user_id: user_id)
-end
+# all_cards.each do |card|
+#   name = card.name
+#   category = card.types.first
+#   rarity = card.rarity
+#   image = card.images.large
+#   price = 9.99
+#   user_id = rand(1..5)
+#   Card.create(name: name, category: category, rarity: rarity, image: image, price: price, user_id: user_id)
+# end
 
 cards_db = Pokemon::Card.where(q: 'set.name:base')
 
